38245629589472502d03e4473096ccffc853c586
r930709 <sunnyboyr930709@gmail.com>
2015-10-28 23:49:00 +0800

    Complete q2 recursive version & generate char series[auto]

--- q2/main.c
+++ q2/main.c
@@ -22,27 +22,24 @@ static double diff_in_second(struct timespec t1, struct timespec t2)
 
 int main()
 {
-    char line[64],*str,ser[64];
     int ch = 0;
     char c,result;
     struct timespec start,end;
     double cpu_time1;
-    FILE *fp;
     int i=0;
-    fp = fopen("character_series.txt","r");
-    fgets(line,sizeof(line),fp);
-    str = strtok(line,",");
-    while(str!=NULL) {
-        ser[i] = str[0];
-        i++;
-        str = strtok(NULL,",");
-    }
-    ser[i] = '\0';
-    i=0;
-    while(ser[i]!='\0') {
-        printf("show %c\n",ser[i]);
+    char charBank[27] = "abcdefghijklmnopqrstuvwxyz";
+    char str[26 * 1 + 1];
+    int len = 0;
+    srand(time(NULL));
+    while(i<=26*1) {
+        if(rand() & 1) {
+            str[len++] = charBank[i];
+        }
         i++;
     }
+    str[len++] = '\0';
+    printf("%s\n",str);
+
     while(1) {
         printf("<1>input a character <2>Exit : ");
         scanf("%d",&ch);
@@ -51,29 +48,28 @@ int main()
         } else if(ch==1) {
             printf("input a character: ");
             scanf(" %c",&c);
-            /*
-            #if defined(RECUR)
-            	clock_gettime(CLOCK_REALTIME, &start);
-                Flatten_Recursive(root);
-            	clock_gettime(CLOCK_REALTIME, &end);
-            	cpu_time1 = diff_in_second(start,end);
-            #endif
-            */
 #if defined(ITER)
             clock_gettime(CLOCK_REALTIME, &start);
-            result = Smallest_Character_Iterative(ser,c);
+            result = Smallest_Character_Iterative(str,c);
             clock_gettime(CLOCK_REALTIME, &end);
             cpu_time1 = diff_in_second(start,end);
 #endif
-            /*
-            #if defined(RECUR)
-            	printf("execution time of Flatten_Recursive() %.10lf sec\n",cpu_time1);
-            #endif
-            */
+#if defined(RECUR)
+            clock_gettime(CLOCK_REALTIME, &start);
+            result = Smallest_Character_Recursive(str,c);
+            clock_gettime(CLOCK_REALTIME, &end);
+            cpu_time1 = diff_in_second(start,end);
+#endif
+#if defined(RECUR)
+            printf("result = [ %c ]\n",result);
+            printf("execution time of Recursive() %.10lf sec\n",cpu_time1);
+#endif
+
 #if defined(ITER)
             printf("result = [ %c ]\n",result);
-            printf("execution time of Flatten_Iterative() %.10lf sec\n",cpu_time1);
+            printf("execution time of Iterative() %.10lf sec\n",cpu_time1);
 #endif
+
         }
     }
 
--- q2/recursive.c
+++ q2/recursive.c
@@ -1,6 +1,42 @@
-/* FIXME: Implement! */
+#include<stdio.h>
 
-int main()
+int mystrlen(char str[])
 {
-    return 0;
+    int count = 0;
+    while(str[count]!='\0') {
+        count++;
+    }
+    return(count);
 }
+char Small_Ch(char str[], char c,int head,int tail)
+{
+
+    int mid = (head+tail)/2;
+    if(c == str[mid]) {
+        while(c==str[++mid]);
+        return(str[mid]);
+    } else if(c > str[mid]) {
+        if(c < str[mid+1]) {
+            return(str[mid+1]);
+        }
+        Small_Ch(str,c,mid+1,tail);
+    } else if(c < str[mid]) {
+        Small_Ch(str,c,head,mid);
+    }
+}
+char Smallest_Character_Recursive(char str[], char c)
+{
+
+    printf("%s\n",str);
+    int tail = mystrlen(str);
+    printf("%d\n",tail);
+    int head = 0;
+    if(c >= str[tail-1]) {
+
+        return(str[0]);
+    } else {
+
+        return(Small_Ch(str,c,head,tail));
+    }
+}
+
